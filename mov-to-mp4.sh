e5c50be7b5429c394044d081d7c7b8b454c39a34
Ryan Auger <ryanaugercc@gmail.com>
2020-03-06 20:32:36 -0500

    Simple helpful

---
+++ mov-to-mp4.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+
+ffmpeg -i $1 -q:v 0 $2
+
---
+++ mp4-to-gif.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+
+ffmpeg -i $1 -vf "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 $2
+
